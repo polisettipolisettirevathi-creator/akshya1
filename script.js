f8a62b4e95499e74862dbc2ea04a52000fee3783
polisettipolisettirevathi-creator <polisettipolisettirevathi@gmail.com>
2026-07-29 15:59:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const menuBtn = document.querySelector(".menu-btn");
+const navLinks = document.querySelector(".nav-links");
+
+menuBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
+
+// Smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+
+    anchor.addEventListener("click", function(e){
+
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute("href"))
+        .scrollIntoView({
+            behavior:"smooth"
+        });
+
+        navLinks.classList.remove("active");
+
+    });
+
+});
